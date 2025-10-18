8f3cb06ae3f2c11e3921c79b0d4c3f2ce7d1c792
aryan7071 <aryan002005@gmail.com>
2025-10-19 01:04:12 +0530

    fixed

--- app/shorten/page.js
+++ app/shorten/page.js
@@ -1,12 +1,11 @@
 "use client"
-import { Code } from 'mongodb'
-import React from 'react'
-import { useState } from 'react'
+import Link from 'next/link'
+import React, { useState } from 'react'
 
 const Shorten = () => {
   const [url, seturl] = useState("")
   const [shorturl, setshorturl] = useState("")
-  const [generated, setgenerated] = useState("")
+  const [generated, setGenerated] = useState("")
 
   const generate = () => {
     const myHeaders = new Headers();
@@ -27,41 +26,37 @@ const Shorten = () => {
     fetch("/api/generate", requestOptions)
       .then((response) => response.json())
       .then((result) => {
-        setgenerated(`${process.env.NEXT_PUBLIC_HOST}/${shorturl}`)
+        setGenerated(`${process.env.NEXT_PUBLIC_HOST}/${shorturl}`)
         seturl("")
         setshorturl("")
-        console.log(result)
+        console.log("This is result",result)
         alert(result.message)
-      
+
       })
       .catch((error) => console.error(error));
   }
 
 
   return (
-    <div className='mx-auto max-w-lg bg-purple-100 my-16 p-8 rounded-lg flex flex-col gap-4 ' >
-      <h1 className='font-bold text-2xl' >Generate Your short URLs</h1>
-      <div className='flex flex-col gap-2 ' >
+    <div className='mx-auto max-w-lg bg-purple-100 my-16 p-8 rounded-lg flex flex-col gap-4'>
+      <h1 className='font-bold text-2xl'>Generate your short URLs</h1>
+      <div className='flex flex-col gap-2'>
         <input type="text"
           value={url}
-          className='px-4 py-2 focus:outline-purple-600 bg-white rounded-md'
+          className='px-4 py-2 focus:outline-purple-600 rounded-md'
           placeholder='Enter your URL'
           onChange={e => { seturl(e.target.value) }} />
 
         <input type="text"
           value={shorturl}
-          className='px-4 py-2 focus:outline-purple-600 bg-white rounded-md '
-          placeholder='Enter preferred short URL text'
+          className='px-4 py-2 focus:outline-purple-600 rounded-md'
+          placeholder='Enter your preferred short URL text'
           onChange={e => { setshorturl(e.target.value) }} />
-
-        <button onClick={generate} className='bg-purple-500 shadow-lg p-2 py-1 rounded-lg font-bold text-white my-3' >Generate</button>
+        <button onClick={generate} className='bg-purple-500 rounded-lg shadow-lg p-3 py-1 my-3 font-bold text-white'>Generate</button>
       </div>
 
-      {generated && <>
-       <span className='font-bold' >Your Link</span> <code> <Link target="_blank" href={generated}>{generated}</Link> 
-        </code></>}
-
-
+      {generated && <> <span className='font-bold text-lg'>Your Link </span><code><Link target="_blank" href={generated}>{generated}</Link>
+      </code></>}
     </div>
   )
 }
